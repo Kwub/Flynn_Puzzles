b070b3a9389a2aabb0915b8bd2500c46a660b9a2
Kwub <50953326+Kwub@users.noreply.github.com>
2019-09-26 18:45:00 -0400

    updat

--- flynnriddle/src/flynnriddle/adjective.java
+++ flynnriddle/src/flynnriddle/adjective.java
@@ -7,19 +7,34 @@ public class adjective {
 
 	public static void main(String[] arg) {
 		try {
-			URL url = new URL("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+			URL url = new URL("http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b");
 			BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
 
 			String inputline;
+			int counter = 1;
 			while ((inputline = read.readLine()) != null) {
-				if ((inputline.length() == 5) && (inputline.substring(inputline.length() - 1).equals("y")) && (inputline.contains("o")))
-					System.out.println(inputline);
+				if (counter >= 127) {
+					String[] line = inputline.split(" ");
+					String word = line[0];
+					if ((word.length() == 5) && (word.substring(word.length() - 1).equals("y")) && (checkforOW(line)))
+						System.out.println(inputline);
+				}
+				counter++;
 			}
 
 			read.close();
 		} catch (IOException e) {
 			System.out.println("sorry");
 		}
+	}
+
+	public static boolean checkforOW(String[] s) {
+		for (int i = 1; i < s.length; i++) {
+			if (s[i].equals("OW")) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
