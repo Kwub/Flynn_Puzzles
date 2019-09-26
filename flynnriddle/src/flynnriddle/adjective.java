439d861e63e828de6b5d3d677b371c055c9edc3b
Kwub <50953326+Kwub@users.noreply.github.com>
2019-09-26 17:30:27 -0400

    update

--- flynnriddle/src/flynnriddle/adjective.java
+++ flynnriddle/src/flynnriddle/adjective.java
@@ -3,23 +3,23 @@ package flynnriddle;
 import java.net.*;
 import java.io.*;
 
-
 public class adjective {
-	
-	public static void main(String[]arg) {
+
+	public static void main(String[] arg) {
 		try {
-		URL url = new URL("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
-		BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
-		
-		String inputline;
-		while((inputline = read.readLine()) != null) {
-			System.out.println(inputline);
-		}
-		
-		read.close();
-		}
-		catch(IOException e) {
+			URL url = new URL("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+			BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
+
+			String inputline;
+			while ((inputline = read.readLine()) != null) {
+				if ((inputline.length() == 5) && (inputline.substring(inputline.length() - 1).equals("y")))
+					System.out.println(inputline);
+			}
+
+			read.close();
+		} catch (IOException e) {
 			System.out.println("sorry");
 		}
 	}
+
 }
